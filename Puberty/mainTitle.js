6ed1a1ecf0ecda68956346eafc9f2f1484294496
jeremysiegel <jeremysiegel2@gmail.com>
2022-04-03 16:21:21 -0400

    show changes title only

--- Puberty/mainTitle.js
+++ Puberty/mainTitle.js
@@ -1,28 +1,27 @@
 // This scene is called on the initial start of the application and any manual url change.
 
-import TitleButton from '../../../common/js/titleButton.js';
-import BaseScene from './base.js';
+import TitleButton from "../../../common/js/titleButton.js";
+import BaseScene from "./base.js";
 
 export default class MainTitle extends BaseScene {
   constructor() {
-    super('mainTitle');
+    super("mainTitle");
   }
 
-  preload () {
+  preload() {
     super.preload();
   }
 
-  create () {
-   
-    super.create('blueBackground');
+  create() {
+    super.create("blueBackground");
 
     var sceneButtons = {
       changesButton: {
-        color: '0x9a79ff',
-        text: 'New You:\nChanges, Thoughts, and Feelings',
-        nextScene: 'changesTitle'
+        color: "0x9a79ff",
+        text: "New You:\nChanges, Thoughts, and Feelings",
+        nextScene: "changesTitle",
       },
-
+      /*
       reproductiveButton: {
         color: '0x00cc34',
         text: 'The Reproductive System',
@@ -34,17 +33,44 @@ export default class MainTitle extends BaseScene {
         text: 'All About Pregnancy & Periods',
         nextScene: 'periodTitle'
       }
-    }
-    
+      */
+    };
+
     var spacing = 30;
     var width = 450;
     var height = 250;
 
-    this.add.image(194.5, 165.25, 'titleText');
+    this.add.image(194.5, 165.25, "titleText");
 
-    new TitleButton(this, this.cameras.main.width/2 - width/2, this.cameras.main.height/2-height-spacing, width, height + 30, sceneButtons.changesButton.color, sceneButtons.changesButton.text, sceneButtons.changesButton.nextScene);
-    new TitleButton(this, this.cameras.main.width/2-width-spacing, this.cameras.main.height/2+spacing, width, height, sceneButtons.reproductiveButton.color, sceneButtons.reproductiveButton.text, sceneButtons.reproductiveButton.nextScene);
-    new TitleButton(this, this.cameras.main.width/2+spacing, this.cameras.main.height/2+spacing, width, height, sceneButtons.periodButton.color, sceneButtons.periodButton.text, sceneButtons.periodButton.nextScene);
-
+    new TitleButton(
+      this,
+      this.cameras.main.width / 2 - width / 2,
+      this.cameras.main.height / 2 - height - spacing,
+      width,
+      height + 30,
+      sceneButtons.changesButton.color,
+      sceneButtons.changesButton.text,
+      sceneButtons.changesButton.nextScene
+    );
+    new TitleButton(
+      this,
+      this.cameras.main.width / 2 - width - spacing,
+      this.cameras.main.height / 2 + spacing,
+      width,
+      height,
+      sceneButtons.reproductiveButton.color,
+      sceneButtons.reproductiveButton.text,
+      sceneButtons.reproductiveButton.nextScene
+    );
+    new TitleButton(
+      this,
+      this.cameras.main.width / 2 + spacing,
+      this.cameras.main.height / 2 + spacing,
+      width,
+      height,
+      sceneButtons.periodButton.color,
+      sceneButtons.periodButton.text,
+      sceneButtons.periodButton.nextScene
+    );
   }
 }
